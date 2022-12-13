cc7895f2ff5d51920e8e2735741413cb74314b0a
Artem Egorov <Artem-Egorov13@mail.ru>
2022-12-13 15:07:54 +0300

    add findLast with limit test

--- src/test/java/ru/netology/manager/MovieManager/MovieManagerTest.java
+++ src/test/java/ru/netology/manager/MovieManager/MovieManagerTest.java
@@ -62,7 +62,7 @@ class MovieManagerTest {
     }
 
     @Test
-    public void findLastTwelveMovies() {
+    public void findLastTenMovies() {
         MovieManager manager = new MovieManager();
 
         manager.addMovie("Fast & Furious 1");
@@ -80,6 +80,71 @@ class MovieManagerTest {
                 "Fast & Furious 8", "Fast & Furious 7", "Fast & Furious 6", "Fast & Furious 5", "Fast & Furious 4",
                 "Fast & Furious 3", "Fast & Furious 2", "Fast & Furious 1"};
         String [] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void findLastWhenMoviesBelowLimit () {
+        MovieManager manager = new MovieManager(10);
+
+        manager.addMovie("Charly 1");
+        manager.addMovie("Charly 2");
+        manager.addMovie("Charly 3");
+        manager.addMovie("Charly 4");
+
+        String[] expected = {"Charly 4", "Charly 3", "Charly 2", "Charly 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+
+    public void findLastWhenMoviesEqualLimit () {
+        MovieManager manager = new MovieManager(10);
+
+        manager.addMovie("Sin 1");
+        manager.addMovie("Sin 2");
+        manager.addMovie("Sin 3");
+        manager.addMovie("Sin 4");
+        manager.addMovie("Sin 5");
+        manager.addMovie("Sin 6");
+        manager.addMovie("Sin 7");
+        manager.addMovie("Sin 8");
+        manager.addMovie("Sin 9");
+        manager.addMovie("Sin 10");
+
+        String[] expected = {"Sin 10", "Sin 9", "Sin 8", "Sin 7", "Sin 6", "Sin 5", "Sin 4", "Sin 3", "Sin 2", "Sin 1", };
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+
+    public void findLastWhenMoviesOverLimit () {
+        MovieManager manager = new MovieManager(10);
+
+        manager.addMovie("Sally 1");
+        manager.addMovie("Sally 2");
+        manager.addMovie("Sally 3");
+        manager.addMovie("Sally 4");
+        manager.addMovie("Sally 5");
+        manager.addMovie("Sally 6");
+        manager.addMovie("Sally 7");
+        manager.addMovie("Sally 8");
+        manager.addMovie("Sally 9");
+        manager.addMovie("Sally 10");
+        manager.addMovie("Sally 11");
+
+        String[] expected = {"Sally 11", "Sally 10", "Sally 9", "Sally 8", "Sally 7", "Sally 6", "Sally 5",
+                "Sally 4", "Sally 3", "Sally 2"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
 
     }
 }
